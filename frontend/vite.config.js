2891df4415f886be6720f01cb724eeb2f0c4b917
서민석 <http://github.com/mickseogi>
2025-05-14 11:42:27 +0900

    페이지 출력 확인

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: './',             // 현재 디렉토리 기준 (frontend 안에서 실행 중이니까 그대로)
+  publicDir: 'public',    // public 폴더 경로 명시
   plugins: [react()],
 })
